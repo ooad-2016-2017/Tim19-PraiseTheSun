0ec6bb0faa182cc28fbbeae1e0e06036aa6bdea1
ATalic2 <atalic2@etf.unsa.ba>
2017-06-06 04:44:38 +0200

    evo

--- New Unity Project/Assets/Spawn.cs
+++ New Unity Project/Assets/Spawn.cs
@@ -15,7 +15,7 @@ public class Spawn : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        if (Input.GetKeyDown("space"))
+        if (Input.GetKeyDown("enter"))
         {
 
             startPosition = transform.position;
--- New Unity Project/Assets/destroy.cs
+++ New Unity Project/Assets/destroy.cs
@@ -6,10 +6,22 @@ public class destroy : MonoBehaviour {
 
     //public GameObject me;
 
-    void OnTriggerEnter(Collider collider)
+    void Update()
     {
-        
+        /*if (Input.GetKeyDown("space"))
+        {
             Destroy(gameObject);
-        
+        }*/
+        var a=GameObject.FindGameObjectWithTag("Xander").transform.position;
+        Vector3 b= new Vector3(0, 0, 0);
+        if (a.x>-0.5 && a.x<0.5 && a.y<0.5 && a.y > -0.5 && Input.GetKey("space"))
+        {
+            /*if (Input.GetKey("space"))
+            {*/
+
+                Destroy(gameObject);
+            //}
+            
+        }
     }
 }
