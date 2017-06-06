fbbeba87d4e825b3843f7627810e5b7846144180
ATalic2 <atalic2@etf.unsa.ba>
2017-06-06 04:17:29 +0200

    unity

---
+++ New Unity Project/Assets/Spawn.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawn : MonoBehaviour {
+    private Vector3 startPosition;
+    public float moveSpeed = 1f;
+    public float moveDistance = 4f;
+    public GameObject randomGO;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown("space"))
+        {
+
+            startPosition = transform.position;
+            GameObject myObj = Instantiate(randomGO) as GameObject;
+            myObj.transform.position = transform.position;
+        }
+    }
+}
---
+++ New Unity Project/Assets/cdestroz.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cdestroz : MonoBehaviour {
+
+    void OnTriggerEnter(Collider collider)
+    {
+
+        Destroy(gameObject);
+        Debug.Log("fhg");
+    }
+}
